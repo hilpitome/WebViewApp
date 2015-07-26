4413f0c4871ea2133318afb7c555d38c4da4aec2
hilpitome <hilpitome@gmail.com>
2015-07-26 22:45:10 +0300

    completed webview

--- app/src/main/java/hilary/moringa/com/webviewapp/MainActivity.java
+++ app/src/main/java/hilary/moringa/com/webviewapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +13,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String url = "http//teknorial.com/";
+        WebView View = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+    }
+git 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
